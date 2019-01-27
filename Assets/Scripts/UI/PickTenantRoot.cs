d9d200629d8935dc1c0b34a1eef2367378494957
Marijn Zwemmer <marijn@marijnzwemmer.com>
2019-01-27 01:11:20 +0100

    Removed old code

--- Assets/Scripts/UI/PickTenantRoot.cs
+++ Assets/Scripts/UI/PickTenantRoot.cs
@@ -7,97 +7,97 @@ using UnityEngine.SceneManagement;
 
 public class GeneratedTenant
 {
-	public List<TenantTrait> traits = new List<TenantTrait>();
-	public TenantData.TenantItem data;
+    public List<TenantTrait> traits = new List<TenantTrait>();
+    public TenantData.TenantItem data;
 }
 
 public class PickTenantRoot : MonoBehaviour
 {
-	public static List<GeneratedTenant> results = new List<GeneratedTenant>();
+    public static List<GeneratedTenant> results = new List<GeneratedTenant>();
 
-	const int amountOfTenantsAvailable = 4;
-	const int amountOfTenantsToPick = 3;
-	const string sceneName = "PickTenant";
+    const int amountOfTenantsAvailable = 4;
+    const int amountOfTenantsToPick = 3;
+    const string sceneName = "PickTenant";
 
-	public TenantData tenantData;
-	public TenantGenerator tenantGenerator;
-	public CanvasGroup container;
-	public GameObject tennantsContainer;
-	public TenantElement template;
+    public TenantData tenantData;
+    public TenantGenerator tenantGenerator;
+    public CanvasGroup container;
+    public GameObject tennantsContainer;
+    public TenantElement template;
 
-	public Action<TenantData.TenantItem> onDone;
+    public Action<TenantData.TenantItem> onDone;
 
-	TenantData.TenantItem chosenItem;
+    TenantData.TenantItem chosenItem;
 
-	static bool done;
+    static bool done;
 
-	bool closing;
+    bool closing;
 
-	public static IEnumerator Show()
-	{
-		results.Clear();
-		done = false;
+    public static IEnumerator Show()
+    {
+        results.Clear();
+        done = false;
 
-		SceneManager.LoadScene("PickTenant", LoadSceneMode.Additive);
-		while (!done)
-		{
-			yield return null;
-		}
-	}
+        SceneManager.LoadScene("PickTenant", LoadSceneMode.Additive);
+        while(!done)
+        {
+            yield return null;
+        }
+    }
 
-	IEnumerator CloseScene()
-	{
-		yield return new WaitForSeconds(.3f);
-		container.DOFade(0, .3f).OnComplete(() =>
-		{
-			SceneManager.UnloadSceneAsync(sceneName);
-			done = true;
-		});
-	}
+    IEnumerator CloseScene()
+    {
+        yield return new WaitForSeconds(.3f);
+        container.DOFade(0, .3f).OnComplete(() =>
+        {
+            SceneManager.UnloadSceneAsync(sceneName);
+            done = true;
+        });
+    }
 
-	void Awake()
-	{
-		container.alpha = 0;
-		container.DOFade(1, .3f);
-	}
+    void Awake()
+    {
+        container.alpha = 0;
+        container.DOFade(1, .3f);
+    }
 
-	void Start()
-	{
-		int count = 0;
-		for (int i = 0; i < amountOfTenantsAvailable; i++)
-		{
-			var instance = Instantiate(template, tennantsContainer.transform, false);
-			var data = tenantData.potentialTenants.Random();
-			var generatedTenant = tenantGenerator.Generate(data);
-			instance.Init(generatedTenant);
-			instance.button.onClick += () =>
-			{
-				if (!closing)
-				{
-					if (!instance.selected)
-					{
-						instance.transform.DOScale(Vector3.one * 1.1f, .3f).SetEase(Ease.OutBack);
-						instance.selected = true;
-						results.Add(generatedTenant);
-						count++;
+    void Start()
+    {
+        int count = 0;
+        for (int i = 0; i < amountOfTenantsAvailable; i++)
+        {
+            var instance = Instantiate(template, tennantsContainer.transform, false);
+            var data = tenantData.potentialTenants.Random();
+            var generatedTenant = tenantGenerator.Generate(data);
+            instance.Init(generatedTenant);
+            instance.button.onClick += () =>
+            {
+                if(!closing)
+                {
+                    if(!instance.selected)
+                    {
+                        instance.transform.DOScale(Vector3.one * 1.1f, .3f).SetEase(Ease.OutBack);
+                        instance.selected = true;
+                        results.Add(generatedTenant);
+                        count++;
                         AudioSource.PlayClipAtPoint(data.audioClip, Vector3.zero);
-					}
-					else
-					{
-						instance.transform.DOScale(Vector3.one, .3f).SetEase(Ease.OutSine);
-						instance.selected = false;
-						results.Remove(generatedTenant);
-						count--;
-					}
-					if (count == amountOfTenantsToPick)
-					{
-						closing = true;
-						StartCoroutine(CloseScene());
-					}
-				}
-			};
-		}
+                    }
+                    else
+                    {
+                        instance.transform.DOScale(Vector3.one, .3f).SetEase(Ease.OutSine);
+                        instance.selected = false;
+                        results.Remove(generatedTenant);
+                        count--;
+                    }
+                    if(count == amountOfTenantsToPick)
+                    {
+                        closing = true;
+                        StartCoroutine(CloseScene());
+                    }
+                }
+            };
+        }
 
-		template.gameObject.SetActive(false);
-	}
+        template.gameObject.SetActive(false);
+    }
 }
